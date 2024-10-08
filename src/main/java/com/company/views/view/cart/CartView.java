e39d14ed528371e68d5fbcafb29b3b521f950e61
Sergei Iudaev <s.iudaev@haulmont.com>
2024-10-08 17:07:40 +0400

    Fixes

--- src/main/java/com/company/views/view/cart/CartView.java
+++ src/main/java/com/company/views/view/cart/CartView.java
@@ -3,16 +3,10 @@ package com.company.views.view.cart;
 
 import com.company.views.entity.Product;
 import com.company.views.view.main.MainView;
-import com.company.views.view.user.UserListView;
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.html.NativeLabel;
-import com.vaadin.flow.router.BeforeLeaveEvent;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
-import io.jmix.core.DataManager;
-import io.jmix.core.EntityStates;
 import io.jmix.flowui.Dialogs;
 import io.jmix.flowui.UiComponents;
-import io.jmix.flowui.ViewNavigators;
 import io.jmix.flowui.action.DialogAction;
 import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.kit.component.button.JmixButton;
@@ -20,12 +14,6 @@ import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Route(value = "cart-view", layout = MainView.class)
 @ViewController("CartView")
@@ -35,26 +23,24 @@ public class CartView extends StandardView {
     @ViewComponent
     private DataGrid<Product> productsDataGrid;
     @ViewComponent
-    private NativeLabel cartTotalLabel;
-    @Autowired
-    private UiComponents uiComponents;
+    private Span cartTotalSpan;
 
     @ViewComponent
     private CollectionContainer<Product> productsDc;
     @ViewComponent
     private DataContext dataContext;
+
+    @Autowired
+    private UiComponents uiComponents;
     @Autowired
     private Dialogs dialogs;
-
 
     @Subscribe
     public void onInit(InitEvent event) {
         productsDataGrid.addComponentColumn(product -> {
             JmixButton addToCartButton = uiComponents.create(JmixButton.class);
             addToCartButton.setText("Add to cart");
-            addToCartButton.addClickListener(clickEvent -> {
-                addToCart(product);
-            });
+            addToCartButton.addClickListener(clickEvent -> addToCart(product));
             return addToCartButton;
         });
     }
@@ -68,7 +54,7 @@ public class CartView extends StandardView {
         double total = productsDc.getItems().stream()
                 .mapToDouble(p -> p.getPrice() * p.getAmount())
                 .sum();
-        cartTotalLabel.setText(String.format("Total: $%.2f", total));
+        cartTotalSpan.setText(String.format("Total: $%.2f", total));
     }
 
     private void addToCart(Product product) {
@@ -80,24 +66,19 @@ public class CartView extends StandardView {
     public void onBeforeClose(BeforeCloseEvent event) {
 //        dataContext.save();
 
-        if (dataContext.hasChanges()) {
-            CloseAction action = event.getCloseAction();
-            if (action instanceof NavigateCloseAction navigateCloseAction) {
-                BeforeLeaveEvent beforeLeaveEvent = navigateCloseAction.getBeforeLeaveEvent();
-                beforeLeaveEvent.postpone();
+        if (dataContext.hasChanges() && event.closedWith(StandardOutcome.CLOSE)) {
 
-                dialogs.createOptionDialog()
-                        .withText("Save cart for later?")
-                        .withActions(
-                                new DialogAction(DialogAction.Type.YES)
-                                        .withHandler(e -> {
-                                            dataContext.save();
-                                            close(StandardOutcome.SAVE);}),
-                                new DialogAction(DialogAction.Type.NO)
-                                        .withHandler(e -> close(StandardOutcome.CLOSE))
-                        ).open();
-            }
+            dialogs.createOptionDialog()
+                    .withText("Save cart for later?")
+                    .withActions(
+                            new DialogAction(DialogAction.Type.YES)
+                                    .withHandler(e -> {
+                                        dataContext.save();
+                                        close(StandardOutcome.SAVE);
+                                    }),
+                            new DialogAction(DialogAction.Type.NO)
+                                    .withHandler(e -> close(StandardOutcome.DISCARD))
+                    ).open();
         }
     }
-
 }
